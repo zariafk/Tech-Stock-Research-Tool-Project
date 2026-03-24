1a7174433a589deaa1b190470528a254810215b2
Jess <trainee.jess.ho@sigmalabs.co.uk>
2026-03-24 16:38:19 +0000

    pull lance hcane

--- pipeline/rss/rss_extract.py
+++ pipeline/rss/rss_extract.py
@@ -3,7 +3,6 @@
 Currently fetches news for a predefined list of tech stock tickers from Yahoo Finance RSS feeds."""
 
 from typing import Optional
-import logging
 from datetime import datetime
 import pandas as pd
 import feedparser
@@ -11,7 +10,7 @@ import requests
 from requests.exceptions import RequestException as NetworkError
 from logger import make_logger
 
-logger = make_logger(__name__)
+logger = make_logger()
 
 # List of tech stock tickers to fetch news for
 TECH_TICKERS = [

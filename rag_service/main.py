5e63125fb187ed544495fac173048dbff3b8e04a
zariafk <zariafkhan@gmail.com>
2026-03-27 10:37:19 +0000

    abstracting functionality out of main

--- rag_service/main.py
+++ rag_service/main.py
@@ -6,8 +6,9 @@ Mostly used for local testing currently.
 
 from app.pipeline import ingest_data, answer_query
 
-if __name__ == "__main__":
-    """mostly for testing"""
+
+def main():
+    """to test RAG system locally"""
     ingest_data(source="alpaca", data_path="data/sample_alpaca.json")
     ingest_data(source="rss", data_path="data/sample_rss.json")
     ingest_data(source="reddit", data_path="data/sample_reddit.json")
@@ -15,3 +16,8 @@ if __name__ == "__main__":
     query = "What is happening with NVDA?"
     answer = answer_query(query, sources=["reddit"])
     print(answer)
+
+
+if __name__ == "__main__":
+    """calling testing main function"""
+    main()

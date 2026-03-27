52871f4be3d9d811682176bc93b7c73455d22baa
Lance_Lalsie <trainee.lance.lalsie@sigmalabs.co.uk>
2026-03-27 10:38:37 +0000

    terraform resources changed to suit rds. all should work for now

--- terraform/dashboard/ecs.tf
+++ terraform/dashboard/ecs.tf
@@ -15,7 +15,7 @@ resource "aws_ecs_cluster" "c22_stocksiphon_cluster" {
 }
 
 resource "aws_ecr_repository" "c22_stocksiphon_dashboard" {
-  name = "c22-stocksiphon-dashboard"
+  name = "c22-stocksiphon-dashboard-ecr"
 }
 
 resource "aws_iam_role" "c22_stocksiphon_ecs_execution_role" {
@@ -124,7 +124,7 @@ resource "aws_ecs_service" "c22_stocksiphon_dashboard_service" {
   name            = "c22-stocksiphon-dashboard-service"
   cluster         = aws_ecs_cluster.c22_stocksiphon_cluster.id
   task_definition = aws_ecs_task_definition.c22_stocksiphon_dashboard_task.arn
-  desired_count   = 1
+  desired_count   = 0
   launch_type     = "FARGATE"
 
   network_configuration {

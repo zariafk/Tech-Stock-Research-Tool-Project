1482e5ed3880a5901bddc318bc60f68aece179af
zariafk <zariafkhan@gmail.com>
2026-03-30 09:56:40 +0100

    chroma sg

---
+++ terraform/rag/security.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "chroma_sg" {
+    name        = "c22-stocksiphon-chroma-sg"
+    description = "Security group for Chroma ECS service"
+    vpc_id      = var.vpc_id
+
+    ingress {
+        description = "Allow Chroma HTTP"
+        from_port   = 8000
+        to_port     = 8000
+        protocol    = "tcp"
+        cidr_blocks = [cidrsubnet("10.0.0.0/8", 0, 0)]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "c22-stocksiphon-chroma-sg"
+    }
+}
--- terraform/rag/variables.tf
+++ terraform/rag/variables.tf
@@ -1,3 +1,8 @@
+variable "vpc_id" {
+    description = "VPC ID for Chroma resources"
+    type        = string
+}
+
 variable "subnet_ids" {
     description = "Subnets for ECS/EFS"
     type        = list(string)

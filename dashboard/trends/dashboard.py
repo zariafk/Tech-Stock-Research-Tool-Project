69667ba68628efd2ca2ce1690be2cdb07fe201d8
Sam <samueljfgoddard@gmail.com>
2026-03-31 10:41:42 +0100

    updating env repo name

--- dashboard/trends/dashboard.py
+++ dashboard/trends/dashboard.py
@@ -51,7 +51,7 @@ def get_connection():
         )
 
     # Fall back to Secrets Manager (Lambda / prod)
-    secret = get_secret(os.environ["secrets_repo_name"])
+    secret = get_secret(os.environ["SECRETS_REPO_NAME"])
     return psycopg2.connect(
         host=secret["host"],
         port=secret.get("port", 5432),
